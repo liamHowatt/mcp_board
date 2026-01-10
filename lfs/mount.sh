2e7ce7d7ae389cc8e6e138b766042ac044be64fe
liamHowatt <liamjmh0@gmail.com>
2026-01-10 11:52:31 -0500

    improve lfs mount script

--- lfs/mount.sh
+++ lfs/mount.sh
@@ -1,11 +1,18 @@
 set -e
-LFS_ARGS='
+
+if [ $# -ne 2 ]
+then
+        echo usage: sh mount.sh /path/to/block_device /path/to/mountpoint
+        exit 1
+fi
+
+LFS_ARGS="
     --block_cycles=100
     --lookahead_size=0
-    /dev/mmcblk0
-    /mnt/lfssd
-'
+    $1
+    $2
+"
 $(dirname $0)/littlefs-fuse/lfs --stat $LFS_ARGS
-mkdir -p /mnt/lfssd
+mkdir -p $2
 $(dirname $0)/littlefs-fuse/lfs $LFS_ARGS
-echo mounted at /mnt/lfssd
+echo $1 mounted at $2
